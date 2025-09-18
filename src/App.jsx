57710f37fd88ffc5331757680e33129b23c44162
heisokoye <heisokoye@gmail.com>
2025-09-18 12:26:20 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import './App.css'
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/nav/Navbar'
-import Home from '../pages/Home'
-import About from '../pages/About'
-import Blog from '../pages/Blog'
+import Home from './pages/Home'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Footer from './components/footer/Footer'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
